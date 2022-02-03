0042870eec7f3f5b0525fa0d3b9b013a604f0740
KLDAJI <kldaji@naver.com>
2022-02-03 11:25:37 +0900

    #2 feat: Set search icon listener and visibility of bottom navigation view
    - docs
    https://www.notion.so/by-lazy-lateinit-a3a097d79b004e238a46fa7242059c2b

--- app/src/main/java/com/kldaji/loanclientmanagement/ui/client/ClientsFragment.kt
+++ app/src/main/java/com/kldaji/loanclientmanagement/ui/client/ClientsFragment.kt
@@ -2,6 +2,7 @@ package com.kldaji.loanclientmanagement.ui.client
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.kldaji.loanclientmanagement.R
 import com.kldaji.loanclientmanagement.databinding.FragmentClientsBinding
 import com.kldaji.loanclientmanagement.ui.common.BaseFragment
@@ -10,5 +11,20 @@ class ClientsFragment : BaseFragment<FragmentClientsBinding>(R.layout.fragment_c
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setToolbarIconClickListener()
+    }
+
+    private fun setToolbarIconClickListener() {
+        binding.tbClients.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    this.findNavController()
+                        .navigate(R.id.action_clientsFragment_to_clientSearchFragment)
+                    true
+                }
+                R.id.add_client -> true
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/com/kldaji/loanclientmanagement/ui/common/MainActivity.kt
+++ app/src/main/java/com/kldaji/loanclientmanagement/ui/common/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kldaji.loanclientmanagement.ui.common
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -10,18 +11,28 @@ import com.kldaji.loanclientmanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+    }
+    private val navController by lazy { navHostFragment.findNavController() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setNavController()
+        setBottomNavigationVisibility()
     }
 
     private fun setNavController() {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
-        val navController = navHostFragment.findNavController()
         binding.bnvMain.setupWithNavController(navController)
     }
 
+    private fun setBottomNavigationVisibility() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.clientSearchFragment -> binding.bnvMain.isVisible = false
+                else -> binding.bnvMain.isVisible = true
+            }
+        }
+    }
 }
