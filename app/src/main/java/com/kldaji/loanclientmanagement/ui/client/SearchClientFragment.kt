c9673bf28f3c430b997c6b41bce309ea7d865296
KLDAJI <kldaji@naver.com>
2022-02-03 15:02:35 +0900

    #4 feat: Set cancel and root click listener

--- app/src/main/java/com/kldaji/loanclientmanagement/ui/client/SearchClientFragment.kt
+++ app/src/main/java/com/kldaji/loanclientmanagement/ui/client/SearchClientFragment.kt
@@ -2,12 +2,14 @@ package com.kldaji.loanclientmanagement.ui.client
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.kldaji.loanclientmanagement.R
 import com.kldaji.loanclientmanagement.databinding.FragmentSearchClientBinding
 import com.kldaji.loanclientmanagement.model.data.RecentSearchWord
 import com.kldaji.loanclientmanagement.ui.client.adapter.ClientsAdapter
 import com.kldaji.loanclientmanagement.ui.client.adapter.RecentSearchWordsAdapter
 import com.kldaji.loanclientmanagement.ui.common.BaseFragment
+import com.kldaji.loanclientmanagement.utils.hideKeyBoard
 import com.kldaji.loanclientmanagement.utils.showKeyBoard
 
 class SearchClientFragment :
@@ -18,6 +20,8 @@ class SearchClientFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setEditTextFocus()
+        setCancelClickListener()
+        setRootClickListener()
         setRecentSearchWordsAdapter()
         setSearchResultClientsAdapter()
 
@@ -32,6 +36,19 @@ class SearchClientFragment :
         with(binding.tieSearchClient) {
             requestFocus()
             requireContext().showKeyBoard(this)
+        }
+    }
+
+    private fun setCancelClickListener() {
+        binding.tvSearchClientCancel.setOnClickListener {
+            this.findNavController().popBackStack()
+        }
+    }
+
+    private fun setRootClickListener() {
+        binding.root.setOnClickListener { view ->
+            requireContext().hideKeyBoard(view)
+            requireActivity().currentFocus?.clearFocus()
         }
     }
 
