1fabb7d91126f05dbb81f67f06e6c1ecbf12c1e6
KLDAJI <kldaji@naver.com>
2022-02-03 23:29:44 +0900

    #6 feat: Implement AddClientFragment layout

--- app/src/main/java/com/kldaji/loanclientmanagement/ui/client/AddClientFragment.kt
+++ app/src/main/java/com/kldaji/loanclientmanagement/ui/client/AddClientFragment.kt
@@ -3,6 +3,7 @@ package com.kldaji.loanclientmanagement.ui.client
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.kldaji.loanclientmanagement.R
 import com.kldaji.loanclientmanagement.databinding.FragmentAddClientBinding
 import com.kldaji.loanclientmanagement.ui.common.BaseFragment
@@ -14,5 +15,18 @@ class AddClientFragment : BaseFragment<FragmentAddClientBinding>(R.layout.fragme
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setToolbarIconClickListener()
+    }
+
+    private fun setToolbarIconClickListener() {
+        binding.tbAddClient.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.close -> {
+                    this.findNavController().popBackStack()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
