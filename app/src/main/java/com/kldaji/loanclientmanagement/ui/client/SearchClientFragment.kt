c21ed7ecac152789ceed57f519797f818a48fb98
KLDAJI <kldaji@naver.com>
2022-02-03 11:32:18 +0900

    #2 chore: Rename SearchClientFragment

--- app/src/main/java/com/kldaji/loanclientmanagement/ui/client/SearchClientFragment.kt
+++ app/src/main/java/com/kldaji/loanclientmanagement/ui/client/SearchClientFragment.kt
@@ -3,10 +3,10 @@ package com.kldaji.loanclientmanagement.ui.client
 import android.os.Bundle
 import android.view.View
 import com.kldaji.loanclientmanagement.R
-import com.kldaji.loanclientmanagement.databinding.FragmentClientSearchBinding
+import com.kldaji.loanclientmanagement.databinding.FragmentSearchClientBinding
 import com.kldaji.loanclientmanagement.ui.common.BaseFragment
 
-class ClientSearchFragment: BaseFragment<FragmentClientSearchBinding>(R.layout.fragment_client_search) {
+class SearchClientFragment: BaseFragment<FragmentSearchClientBinding>(R.layout.fragment_search_client) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
