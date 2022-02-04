37382597980b51549c0c6cb011d9f736178c6203
KLDAJI <kldaji@naver.com>
2022-02-04 15:51:55 +0900

    #7 feat: Set Client parcelable

--- app/src/main/java/com/kldaji/loanclientmanagement/model/data/Client.kt
+++ app/src/main/java/com/kldaji/loanclientmanagement/model/data/Client.kt
@@ -1,9 +1,12 @@
 package com.kldaji.loanclientmanagement.model.data
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "client")
 data class Client(
     @PrimaryKey(autoGenerate = true)
@@ -26,7 +29,7 @@ data class Client(
     val loanStartDate: String,
     @ColumnInfo
     val docs: String,
-)
+) : Parcelable
 
 enum class Loan {
     SECURITY, JEONSE
